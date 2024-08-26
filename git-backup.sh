491079fa8dfc0a7242ae7d8225383608bc4c4822
Diego Mariño <diegomarino@users.noreply.github.com>
2024-08-26 14:02:17 -0300

    Logic improvements to avoid errors. Logging added

--- git-backup.sh
+++ git-backup.sh
@@ -1,12 +1,51 @@
 #!/bin/bash
 
+#  Set logging
+LOG_FILE="/tmp/mac-settings-git-backup.log"
+
+log() {
+    echo "$(date '+%Y-%m-%d %H:%M:%S') - $1" | tee -a "$LOG_FILE"
+}
+
 # Source variables from mac-settings.sh
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source "$SCRIPT_DIR/mac-settings.sh"
 
-# Now we can use the BASE_OUTPUT_DIR variable
-cd "$BASE_OUTPUT_DIR" || exit 1
+log "Starting git backup -- $HOSTNAME"
 
-git add .
-git commit -m "Automatic backup: $(date '+%Y-%m-%d %H:%M:%S')"
-git push origin main  # Adjust the branch name if necessary
+# Change to the base backup directory
+cd "$BASE_OUTPUT_DIR" || { log "Error: Couldn't change to $BASE_OUTPUT_DIR"; exit 1; }
+
+# Check if there are local changes
+if ! git diff --quiet HEAD; then
+    # There are local changes, commit them
+    git add . || { log "Error: git ADD failed"; exit 1; }
+    git commit -m "Automatic backup from $HOSTNAME: $(date '+%Y-%m-%d %H:%M:%S')" || {
+        if [ $? -eq 1 ]; then
+            log "No changes to commit"
+        else
+            log "Error: git COMMIT failed"; exit 1
+        fi
+    }
+fi
+
+# Now try to pull
+git pull --rebase origin main || {
+    if [ $? -eq 1 ]; then
+        log "No changes to pull from remote"
+    else
+        log "Error: git PULL failed"; exit 1
+    fi
+}
+
+# Finally, push changes
+git push origin main || {
+    if [ $? -eq 1 ]; then
+        log "No changes to push to remote"
+    else
+        log "Error: git PUSH failed"; exit 1
+    fi
+}
+
+log "Git backup completed successfully"
+log "-------------------------------------------------"
